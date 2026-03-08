583d8a5c86397835eb8b324bd8ce46541e8515c5
Kapil Shrestha <kapilshrestha890@gmail.com>
2026-03-08 14:51:21 +0545

    create user table

---
+++ migrate.ts
@@ -0,0 +1,7 @@
+import { db, connection } from "./src/lib/db/db"
+import { migrate } from "drizzle-orm/postgres-js/migrator"
+
+(async () => {
+    await migrate(db, {migrationsFolder: './drizzle' });
+    await connection.end();
+})();
