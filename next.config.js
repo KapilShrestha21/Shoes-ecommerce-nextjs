19a1127e459dc5f819aa5d5f45cb442627f6915b
Kapil Shrestha <kapilshrestha890@gmail.com>
2026-05-27 16:46:41 +0545

    fifth commit

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@ const nextConfig = {
   /* your other config options here */
 
   experimental: {
-    turbo: {
-      enabled: false
-    }
+    // Only applies the turbo disable flag if we are running locally 'development'
+    turbo: process.env.NODE_ENV === 'development' ? { enabled: false } : undefined
   }
 };
 
