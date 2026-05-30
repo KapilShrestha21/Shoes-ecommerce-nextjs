e00032ae264a79d4aa4d5640065f729b9e5d9fc3
Kapil Shrestha <kapilshrestha890@gmail.com>
2026-05-30 10:33:03 +0545

    fix authOptions

--- src/lib/auth/authOptions.ts
+++ src/lib/auth/authOptions.ts
@@ -14,12 +14,12 @@ export const authOptions: AuthOptions = {
 
                 // to extract data form profile which is from google provider and then store in database
                 const data = {
-                    fname: profile.given_name,
-                    lname: profile.family_name,
+                    fname: profile.given_name || "",
+                    lname: profile.family_name || "",
                     email: profile.email,
                     provider: 'Google',
                     externalId: profile.sub,
-                    image: profile.picture,
+                    image: profile.picture || "",
                 };
 
                 // storing in database
@@ -27,20 +27,36 @@ export const authOptions: AuthOptions = {
                     const user = await db
                         .insert(users)
                         .values(data)
-                        .onConflictDoUpdate({ target: users.email, set: data }) // email could be repeat so target it and set data again to update
+                        .onConflictDoUpdate({
+                            target: users.email,
+                            set: {
+                                fname: data.fname,
+                                lname: data.lname,
+                                image: data.image,
+                                externalId: data.externalId
+                            }
+                        }) // email could be repeat so target it and set data again to update
                         .returning();
 
                     // returning the data to use to store it in cookies/ token  
-                    return {
-                        ...data,
-                        name: data.fname,
-                        id: String(user[0].id),
-                        role: user[0].role,
+                    if (user && user[0]) {
+
+                        return {
+                            ...data,
+                            name: data.fname,
+                            id: String(user[0].id),
+                            role: user[0].role,
+                        }
                     }
+
                 } catch (err) {
                     console.log(err);
                     return {
-                        id: '',
+                        id: profile.sub,
+                        name: profile.name || "",
+                        email: profile.email,
+                        image: profile.picture || "",
+                        role: 'customer',
                     }
                 }
             }
@@ -48,16 +64,16 @@ export const authOptions: AuthOptions = {
     ],
 
     callbacks: {
-        async session({session, token}: { session: any; token: any }) {
+        async session({ session, token }: { session: any; token: any }) {
             if (session.user) {
                 session.user.role = token.role,
-                session.user.id = token.id;
+                    session.user.id = token.id;
             }
 
             return session;
         },
-        
-        async jwt({token, user}: { token: any; user: any}) {
+
+        async jwt({ token, user }: { token: any; user: any }) {
             if (user) {
                 token.role = user.role;
                 token.id = user.id;
